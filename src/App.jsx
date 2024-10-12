24fe23cd9eb135c31223a4380bd3d4bb9c7abab1
Amato Luca <amatoroccoluca@gmail.com>
2024-10-12 19:27:01 +0200

    chore: add Header & Quiz components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Quiz from './components/Quiz';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 };
 
 export default App;
