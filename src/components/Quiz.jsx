c76edd99382ca7fedb28e0b4a2695b2cfac27526
Amato Luca <amatoroccoluca@gmail.com>
2024-10-12 20:10:59 +0200

    chore: add shuffle questions and quiz complete screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswer] = useState([]);
+
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Throfy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswer((prevUserAnswers) => {
@@ -16,7 +31,7 @@ const Quiz = () => {
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
